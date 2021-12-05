95bef79e79db35af03259070f43404045c8c3174
Satyajeet Gaurav <satyajeetmanu@gmail.com>
2021-12-05 13:31:42 +0530

    added code to change imageView's image based on number generated on button click

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import kotlin.random.Random
 
@@ -21,7 +22,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val random = Random.nextInt(6)+1
+        val diceImage : ImageView = findViewById(R.id.dice_image)
+        val imageResourceId = when(Random.nextInt(6)+1){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        diceImage.setImageResource(imageResourceId)
     }
 
 }
